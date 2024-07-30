6f2e9d30c516e3638463b8596116363167a09310
Vintody <143565980+Vintody@users.noreply.github.com>
2024-07-30 13:47:39 +0700

    Add files via upload

--- pasien.sql
+++ pasien.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 23 Jul 2024 pada 09.41
+-- Waktu pembuatan: 30 Jul 2024 pada 07.53
 -- Versi server: 10.1.38-MariaDB
 -- Versi PHP: 7.3.2
 
@@ -21,6 +21,46 @@ SET time_zone = "+00:00";
 --
 -- Database: `pasien`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `idpasien` int(50) NOT NULL,
+  `nmpasien` varchar(50) NOT NULL,
+  `jk` varchar(11) NOT NULL,
+  `alamat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pasien`
+--
+
+INSERT INTO `pasien` (`idpasien`, `nmpasien`, `jk`, `alamat`) VALUES
+(9, 'benet', 'Kardiologi', '09');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`idpasien`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `pasien`
+--
+ALTER TABLE `pasien`
+  MODIFY `idpasien` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
